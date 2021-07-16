7184f5de5c1721d22ac7b24eeee502f4d9c325e7
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2021-07-15 23:37:55 -0300

    Preparing deploy

--- api/src/index.js
+++ api/src/index.js
@@ -6,13 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', false);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  res.setHeader('Content-Type', 'application/json');
-  next();
+
+app.get('/api', (req, res) => {
+  return res.send({ status: 'live' });
 });
 
 require('./app/controllers/authController')(app);
@@ -23,11 +19,6 @@ app.use(express.static(__dirname + '/../../dist/'));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../../dist/index.html'));
-});
-
-app.get('/api', (req, res) => {
-  console.log('live');
-  return res.send({ status: 'live' });
 });
 
 app.listen(process.env.PORT || 3000, () => {
